80e6f73a2cfd880f6b9baedf9104dcdf4766479b
Gabriel <gabrielstefan04@gmail.com>
2024-03-25 00:36:14 +0200

    Tema 1 nefinalizata

--- main.cpp
+++ main.cpp
@@ -1,136 +1,122 @@
 #include <iostream>
 #include <array>
 #include <chrono>
-#include <thread>
+//#include <thread>
+//#include <SFML/Graphics.hpp>
+//#include <Helper.h>
+//#include "env_fixes.h"
 
-#include <SFML/Graphics.hpp>
-
-#include <Helper.h>
-
-//////////////////////////////////////////////////////////////////////
-/// NOTE: this include is needed for environment-specific fixes     //
-/// You can remove this include and the call from main              //
-/// if you have tested on all environments, and it works without it //
-#include "env_fixes.h"                                              //
-//////////////////////////////////////////////////////////////////////
-
-
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
+class User{
+private:
+    std::string username;
+    std::string parola;
 public:
-    explicit SomeClass(int) {}
+    User() = default;
+
+    User(const std::string& _username, const std::string& _parola)
+        :username(_username), parola(_parola)
+    {}
+
+    User& operator=(const User& other){
+        username = other.username;
+        parola = other.parola;
+        return *this;
+    }
+
+    User(const User& other)
+    {
+        username = other.username;
+        parola = other.parola;
+    }
+
+    ~User(){}
+
+    friend std::ostream& operator<<(std::ostream& os, const User& other) {
+        os << "Username: " << other.username << ", Parola: " << other.parola << "\n";
+        return os;
+    }
 };
 
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
+class Actor{
+private:
+    std::string nume;
+    int varsta;
+    std::vector<std::string> filmeJucate;
+public:
+    Actor() = default;
 
+    Actor(const std::string& _nume, const int _varsta, const std::vector<std::string>& _filmeJucate)
+            :nume(_nume), varsta(_varsta), filmeJucate(_filmeJucate)
+    {}
+
+    Actor& operator=(const Actor& other){
+        nume = other.nume;
+        varsta = other.varsta;
+        filmeJucate = other.filmeJucate;
+        return *this;
+    }
+
+    Actor(const Actor& other)
+    {
+        nume = other.nume;
+        varsta = other.varsta;
+    }
+
+    ~Actor(){}
+
+    friend std::ostream& operator<<(std::ostream& os, const Actor& other) {
+        os << "Nume actor: " << other.nume << ", Varsta: " << other.varsta << "\n";
+        os << "Filme jucate: ";
+        for (const std::string& film : other.filmeJucate) {
+            os << film << ", ";
+        }
+        os << "\n";
+        return os;
+    }
+};
+
+class Film{
+private:
+    std::string nume;
+    int durata;
+    std::vector<Actor> cast;
+public:
+    Film() = default;
+
+    Film(const std::string& _nume, const int _durata, const std::vector<Actor>& _cast)
+            :nume(_nume), durata(_durata), cast(_cast)
+    {}
+
+    Film& operator=(const Film& other){
+        nume = other.nume;
+        durata = other.durata;
+        cast = other.cast;
+        return *this;
+    }
+
+    Film(const Film& other)
+    {
+        nume = other.nume;
+        durata = other.durata;
+        cast = other.cast;
+    }
+
+    ~Film(){}
+
+    friend std::ostream& operator<<(std::ostream& os, const Film& other) {
+        os << "Nume film: " << other.nume << ", Durata: " << other.durata << "\n";
+        os << "Actori: ";
+        for (const std::string& actor : other.cast) {
+            os << actor << ", ";
+        }
+        return os;
+    }
+};
 
 int main() {
-    ////////////////////////////////////////////////////////////////////////
-    /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
-    ////////////////////////////////////////////////////////////////////////
-    ///
-    std::cout << "Hello, world!\n";
-    std::array<int, 100> v{};
-    int nr;
-    std::cout << "Introduceți nr: ";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-    std::cin >> nr;
-    /////////////////////////////////////////////////////////////////////////
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "v[" << i << "] = ";
-        std::cin >> v[i];
-    }
-    std::cout << "\n\n";
-    std::cout << "Am citit de la tastatură " << nr << " elemente:\n";
-    for(int i = 0; i < nr; ++i) {
-        std::cout << "- " << v[i] << "\n";
-    }
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-    ///                Exemplu de utilizare cod generat                     ///
-    ///////////////////////////////////////////////////////////////////////////
-    Helper helper;
-    helper.help();
-    ///////////////////////////////////////////////////////////////////////////
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
-
-    sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                            ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    while(window.isOpen()) {
-        bool shouldExit = false;
-        sf::Event e{};
-        while(window.pollEvent(e)) {
-            switch(e.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New width: " << window.getSize().x << '\n'
-                          << "New height: " << window.getSize().y << '\n';
-                break;
-            case sf::Event::KeyPressed:
-                std::cout << "Received key " << (e.key.code == sf::Keyboard::X ? "X" : "(other)") << "\n";
-                if(e.key.code == sf::Keyboard::Escape)
-                    shouldExit = true;
-                break;
-            default:
-                break;
-            }
-        }
-        if(shouldExit) {
-            window.close();
-            break;
-        }
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(300ms);
-
-        window.clear();
-        window.display();
-    }
+    Actor ac("Mihai", 12, {"Palm Angels", "Miron"});
+    Actor ab;
     return 0;
 }
+
